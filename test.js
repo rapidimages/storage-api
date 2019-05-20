37958366edf994f7da03126b330a9e7aad82d494
James Kyburz <james.kyburz@gmail.com>
2019-05-20 17:36:48 +0200

    tests now using tap

--- test.js
+++ test.js
@@ -1,79 +1,97 @@
-const concat = require('concat-stream')
-const test = require('tape')
+const tap = require('tap')
 const { spawn } = require('child_process')
 const path = require('path')
 const client = require('./packages/storage-api-client')('http://localhost:5000')
 const fs = require('fs')
 const rimraf = require('rimraf')
-const crypto = require('crypto')
 const http = require('http')
 
-const get = (url, cb) => {
-  const request = http.get(url, (res) => {
-    if (res.statusCode !== 200) return cb(new Error('error'))
-    res.pipe(concat((data) => cb(null, data)))
-    res.resume()
-  })
-  request.on('error', cb)
+tap.cleanSnapshot = s => {
+  return s.replace(/"path":"[^"]*"/g, 'temporary-path')
 }
 
-const LICENSE = fs.readFileSync('LICENSE', 'utf-8')
-const LICENSE_HASH = crypto.createHash('sha1').update(LICENSE).digest('hex')
+const { test } = tap
+
+const get = url =>
+  new Promise((resolve, reject) => {
+    const request = http.get(url, res => {
+      if (res.statusCode !== 200) return reject(new Error('error'))
+      let buffer = ''
+      res.on('data', chunk => {
+        buffer += chunk
+      })
+      res.on('end', () => resolve(buffer.toString()))
+      res.on('error', reject)
+      res.resume()
+    })
+    request.on('error', reject)
+  })
 
 process.env.PORT = 5000
-process.env.STORAGE_PATH = path.join(require('os').tmpdir(), 'storage', new Date().getTime().toString(36))
+process.env.STORAGE_PATH = path.join(
+  require('os').tmpdir(),
+  'storage',
+  new Date().getTime().toString(36)
+)
 
-const server = spawn('node', ['./packages/storage-api-server/index.js'], { env: process.env, stdio: process.env.DEBUG ? 'inherit' : '' })
+const server = spawn('node', ['./packages/storage-api-server'], {
+  env: process.env,
+  stdio: process.env.DEBUG ? 'inherit' : ''
+})
 
 process.on('exit', server.kill.bind(server))
 
-test('server is running', (t) => {
+test('server is running', t => {
   ;(function poll () {
-    get('http://localhost:5000/ping', (err, data) => {
-      if (err) return setTimeout(poll, 200)
-      t.end()
+    get('http://localhost:5000/ping').then(t.end, () => {
+      setTimeout(poll, 200)
     })
   })()
 })
 
-test('upload this file to storage', (t) => {
-  client.upload([fs.createReadStream('LICENSE')], {
-    onUploadProgress () { t.ok(true, 'onUploadProgress called') },
-    onHashProgress () { t.ok(true, 'onHashProgress called') },
-    onRequest () { t.ok(true, 'onRequest called') },
-    onUnknown (unknown) { t.ok(unknown.LICENSE, 'file will be uploaded') }
+test('upload this file to storage', async t => {
+  const hash = await client.upload([fs.createReadStream('LICENSE')], {
+    onUploadProgress () {
+      t.ok(true, 'onUploadProgress called')
+    },
+    onHashProgress () {
+      t.ok(true, 'onHashProgress called')
+    },
+    onRequest () {
+      t.ok(true, 'onRequest called')
+    },
+    onUnknown (unknown) {
+      t.ok(unknown.LICENSE, 'file will be uploaded')
+    }
   })
-  .then((hash) => {
-    t.equals(hash.toString(), 'ff7bdf679d697b88c47436aba24b9136c046da92', 'upload manifest hash')
-    t.end()
-  })
+
+  t.matchSnapshot(hash.toString())
 })
 
-test('uploading same file again just gets manifest', (t) => {
-  t.plan(2)
-  client.upload([fs.createReadStream('LICENSE')], {
-    onUnknown (unknown) { t.equals(Object.keys(unknown).length, 0, 'file known by server') }
+test('uploading same file again just gets manifest', async t => {
+  const hash = await client.upload([fs.createReadStream('LICENSE')], {
+    onUnknown (unknown) {
+      t.equals(Object.keys(unknown).length, 0, 'file known by server')
+    }
   })
-  .then((hash) => {
-    t.equals(hash.toString(), 'ff7bdf679d697b88c47436aba24b9136c046da92', 'still same hash')
-  })
+
+  t.matchSnapshot(hash.toString())
 })
 
-test('check manifest for upload', (t) => {
-  get('http://localhost:5000/get/ff7bdf679d697b88c47436aba24b9136c046da92.json', (err, data) => {
-    t.error(err, 'got manifest')
-    const manifest = JSON.parse(data)
-    t.equals(manifest.files[0].key, '81fd98ae93fd5e0a79ebca20ec8881478fe402a8', 'manifest file')
-    get('http://localhost:5000/get/81fd98ae93fd5e0a79ebca20ec8881478fe402a8.md', (err, data) => {
-      t.error(err, 'got license')
-      t.equals(data.toString(), LICENSE, 'license data')
-      t.equals(crypto.createHash('sha1').update(data).digest('hex'), LICENSE_HASH, 'license hash')
-      t.end()
-    })
-  })
+test('check manifest for upload', async t => {
+  t.matchSnapshot(
+    await get(
+      'http://localhost:5000/get/ff7bdf679d697b88c47436aba24b9136c046da92.json'
+    )
+  )
+  t.matchSnapshot(
+    await get(
+      'http://localhost:5000/get/81fd98ae93fd5e0a79ebca20ec8881478fe402a8.md'
+    )
+  )
 })
 
-test('cleanup', (t) => {
+test('cleanup', t => {
   server.kill()
   rimraf(process.env.STORAGE_PATH, t.end)
 })
