4a658b5bbaab020796d5be60c7aaff47fb33acb4
James Kyburz <james.kyburz@gmail.com>
2019-06-10 09:52:39 +0200

    update rollup

--- packages/storage-api-client/config/rollup.config.cjs.js
+++ packages/storage-api-client/config/rollup.config.cjs.js
@@ -1,7 +1,9 @@
 import config from './rollup.config'
 
 export default config({
-  format: 'cjs',
-  dest: 'dist/storage-api-client.cjs.js',
+  output: {
+    format: 'cjs',
+    file: 'dist/storage-api-client.cjs.js'
+  },
   browser: false
 })
