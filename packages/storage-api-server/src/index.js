397f0caeaff1939c6ffeb64881346941926cb2ea
James Kyburz <james.kyburz@gmail.com>
2019-08-05 12:46:00 +0200

    standard formatting fix

--- packages/storage-api-server/src/index.js
+++ packages/storage-api-server/src/index.js
@@ -2,5 +2,5 @@ const server = require('server-base')
 const routes = require('./routes')
 
 server(routes)
-.config.assert(['PORT', 'STORAGE_PATH'])
-.start()
+  .config.assert(['PORT', 'STORAGE_PATH'])
+  .start()
